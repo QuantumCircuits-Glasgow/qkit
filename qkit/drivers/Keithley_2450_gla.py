27f17ed03ab7106e07e7c6ce806f6a1fa3a82ab5
Ch1sha <111579379+Ch1sha@users.noreply.github.com>
2023-02-23 18:03:14 +0000

    Re-updated Keithley
    re-forked the repo, and made the same changes

--- qkit/drivers/Keithley_2450_gla.py
+++ qkit/drivers/Keithley_2450_gla.py
@@ -223,6 +223,155 @@ class Keithley_2450_gla(Instrument):
             self._visainstrument.write(":SENS:VOLT:RANG:AUTO ON")
         else:
             self._visainstrument.write(":SENS:VOLT:RANG:AUTO OFF")
+
+    bufferElementTable = {#table of the possible options for bufferElements when used (specified in manual)
+        "DATE": "The date when the data point was measured",
+        "FORM":"The measured value as it appears on the front panel",
+        "FRAC":"The fractional seconds for the data point when the data point was measured",
+        "READ":"The measurement reading based on the SENS:FUNC setting; if no buffer elements are defined, this option is used",
+        "REL":"The relative time when the data point was measured",
+        "SEC":"The seconds in UTC (Coordinated Universal Time) format when the data point was measured",
+        "SOUR":"The source value; if readback is ON, then it is the readback value, otherwise it is the programmed source value",
+        "SOURFORM":"The source value as it appears on the display",
+        "SOURSTAT":"The status information associated with sourcing",
+        "SOURUNIT":"The unit of value associated with the source value",
+        "STAT":"The status information associated with the measurement The time for the data point",
+        "TIME":"The timestamp for the data point",
+        "UNIT":"The unit of measure associated with the measurement"}
+    possibleModes = ["CURR","VOLT","RES"]#keithley can only be in these modes
+    possibleBoundaries=["MIN","MAX","DEF"]
+    def printBufferElementTable(self,includeDetails=False):
+        '''Print out the buffer element table, with an option to include the details of each element'''
+        if includeDetails:
+            print("Possible buffer elements, including descriptions:")
+            for elmt, desc in self.bufferElementTable.items():
+                print("'{element}': {description}".format(element=elmt,description = desc))
+        else:
+            print("Possible buffer elements:")
+            print("\n".join(self.bufferElementTable.keys()))
+
+    def _validateParameters(self, mode:str=None,readingElements:"tuple[str,...]"=None,boundary:str=None)-> "tuple[bool, str]":
+        '''
+        For each scpi function, call this method and provide the relevant parameters to validate entries
+
+        Returns a (bool,str) tuple, where 1st element (bool) indicates true/false for success/failure 
+         and 2nd element (str) indicates description of failure
+        
+        Leave the unwanted parameters = None if you dont want to validate them
+
+        Supported parameters:
+        - readingElements (refer to bufferElementsTable)
+        - mode ('CURR', 'VOLT' 'RES')
+        - boundary ('DEF', 'MIN', 'MAX')
+        '''
+        logging.debug("Validating Parameters")
+        if (mode!=None):
+            mode=mode.upper()
+            if (mode not in self.possibleModes):
+                return (False,"Invalid mode")
+
+        if readingElements!= None or len(readingElements)==0:
+            for i in readingElements:#making sure each buffer element is correct
+                if i.upper() not in self.bufferElementTable:
+                    return (False,"Invalid Buffer Element")
+        
+        if (boundary != None) & (boundary.upper() not in self.possibleBoundaries):
+            return (False,"Invalid boundary values")
+
+        return (True,"")
+        #then return true if all checks go fine
+   
+    def get_latest_reading(self,bufferName:str="defbuffer1",*readingElements:str):
+        '''
+        Returns the latest reading from a given/default reading buffer
+
+        - 'bufferName': (optional) name of buffer where 'defbuffer1' is the default buffer
+
+        - 'readingElements': (optional) variadic paramters (up to of 14) determining what aspects of the reading get returned
+            E.g: 'DATE', 'UNIT'
+        
+        Note: if you want to add readingElements, you need to specify bufferName first
+        
+        If you're not sure what bufferName to use, use 'defbuffer1' (default)
+        '''
+        logging.debug("Getting latest reading")
+        #preventive error handling
+        test = self._validateParameters(None,readingElements)
+        if test[0]==False:
+            print("Error:",test[1])
+            return None
+        if len(readingElements)>0:#for correct formatting of query, need to handle differently if no elements passed 
+            return self._visainstrument.query(':FETCh "{bName}", {elements}'.format(bName = bufferName,elements = ", ".join(readingElements)))
+        else:
+            return self._visainstrument.query(':FETCh "{bName}"'.format(bName = bufferName))#just get the plain reading as it is
+    
+
+    def make_measurement(self,mode:str,bufferName:str="defbuffer1",*readingElements:str):
+        '''
+        Makes a measurement using the specified function mode, stores and returns the measurement in a reading buffer
+
+        - 'mode': specify either 'CURR', 'RES', 'VOLT' to get a current, resistance or voltage measurement respectively
+
+        - 'bufferName': (optional) name of buffer where 'defbuffer1' is the default buffer
+
+        - 'readingElements': (optional) variadic paramters (up to 14) determining what aspects of the reading get returned (look up bufferElementTable)
+        
+        Note: 
+
+            - if you want to add readingElements, you need to specify bufferName first
+
+            - If you're not sure what bufferName to use, use 'defbuffer1' (default)
+
+            - the 'mode' parameter will change the measurement function to the specified one, and this change will persist
+        '''
+        logging.debug("Making measurement")
+        test = self._validateParameters(mode,readingElements)
+        if test[0]==False:
+            print("Error:",test[1])
+            return None
+        if len(readingElements)>0:
+            return self._visainstrument.query(':MEAS:{mode}? "{bName}", {elements}'.format(mode=mode, bName = bufferName,elements = ", ".join(readingElements)))
+        else:
+            return self._visainstrument.query(':MEAS:{mode}? "{bName}"'.format(mode=mode,bName=bufferName))
+
+    #Display (:DISPlay) function
+    def set_display_digits(self,numDigits:int,mode:str=None):
+        '''
+        Set the number of digits displayed on the front panel for a given measurement function (changes will persist)
+         - 'mode': specify either 'CURR', 'RES', 'VOLT' to get current, resistance or voltage measurement functions respectively
+                - if mode is set to None, then all 3 measurement functions will be changed 
+         - 'numDigits': the number of digits to set the display to. Use an integer value
+        
+        Note: this does NOT affect the accuracy or speed of measurements.
+        '''
+        logging.debug("Setting display digits")
+        test = self._validateParameters(mode)
+        if test[0]==False:
+            print("Error:",test[1])
+            return None
+        numDigits=str(numDigits)
+        if mode == None:#then all 3 measurement functions get changed
+            self._visainstrument.write(':DISP:DIG {digits}'.format(digits=numDigits))
+        else:
+            self._visainstrument.write(':DISP:{mode}:DIG: {digits}'.format(mode = mode,digits=numDigits))
+    def get_display_digits(self,mode:str,boundary:str=None):
+        '''
+        Get the number of digits dispayed for a given measurement function
+        - 'mode': specify either 'CURR', 'RES', 'VOLT' to get current, resistance or voltage measurement functions respectively
+        - 'boundary': (optional) specify the following string values to get the relevant data:
+                - DEF to get to the default number of digits
+                - MIN to get to the minimum number of digits allowed
+                - MAX to get to the maximum number of digits allowed 
+        '''
+        logging.debug("getting number of digits displayed")
+        test = self._validateParameters(mode,None,boundary)
+        if test[0]==False:
+            print("Error:",test[1])
+            return None
+        if boundary==None:
+            return float(self._visainstrument.query(':DISP:{mode}:DIG?'.format(mode=mode)))
+        else:
+            return float(self._visainstrument.query(':DISP:{mode}:DIG? {boundary}'.format(mode=mode,boundary=boundary)))
               
 
 if __name__ == "__main__":
